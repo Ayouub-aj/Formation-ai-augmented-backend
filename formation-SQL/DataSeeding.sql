3a397cbb5c6e71d1cbca765714492a99f6c1fe05
Ayouub-aj <aigle313@gmail.com>
2026-03-24 22:49:00 +0100

    data seeding/addition to table

---
+++ formation-SQL/DataSeeding.sql
@@ -0,0 +1,10 @@
+INSERT INTO library_books (title, author, published_year, status, price)
+VALUES 
+    ('To Kill a Mockingbird', 'harper lee', 1950, 'available', 15.80),
+    ('1984', 'george orwell', 2003, 'Borrowed', NULL),
+    ('Pride and Prejudice', 'jane austen', 1900, 'Lost', 3.80),
+    ('the great gatsby', 'scott fitzgerald', 1930, 'Available', 20.80),
+    ('The Catcher in the Rye', 'jd salinger', 1958, 'Available', 60.00),
+    ('one piece', 'Eiichiro Oda', 1959, 'Borrowed', 30.80),
+    ('Brave New World', 'aldous huxley', 2000, 'Lost', 15.80),
+    ('the hobbit', 'jrr tolkien', 1951, 'Lost', NULL);
