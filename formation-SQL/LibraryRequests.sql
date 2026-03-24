a499a726130621c805150d9886455ab5787b050e
Ayouub-aj <aigle313@gmail.com>
2026-03-24 22:57:26 +0100

    data seeding/addition to table

---
+++ formation-SQL/LibraryRequests.sql
@@ -0,0 +1,6 @@
+--The Catalog: List all books, but only show the title, author, and price
+SELECT title, author, price FROM library_books;
+
+--The Budget Filter: Find all books that cost between 100 MAD and 300 MAD.
+
+
